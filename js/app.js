805877d140321f834cf86199642abd206e099e5a
MichaelBarrett219 <michaelbarrett000@gmail.com>
2019-09-21 08:04:56 -0400

    interactive rave image simulator

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ var App = function(targetElementId, viewWidth, viewHeight, diamondHeight) {
     //Initialize our Diamond data structure from above
     var diamond = new Diamond(diamondHeight);
     var _startSim = true;
+    var timeDiff;
+    var timeSin;
 
     //Start kicks off our main loop
     var spd = 30;
@@ -43,8 +45,9 @@ var App = function(targetElementId, viewWidth, viewHeight, diamondHeight) {
     }
 
     //Update updates all state in the app
-    var timeDiff;
     var seconds;
+    var x;
+    var y;
     me.update = function() {
 	endTime = new Date();
 	timeDiff = endTime - startTime; //in ms
@@ -63,10 +66,14 @@ var App = function(targetElementId, viewWidth, viewHeight, diamondHeight) {
     //Draw draws the entire app
     me.draw = function() {
 	//Erase previous draw by filling entire canvas with white
-	me.ctx.fillStyle = "rgba(0, 0, 0, 0)";
-	me.ctx.fillStyle = 'black';
-	me.ctx.fillStyle = "rgba(0, 0, 0, 0.001)";
-	me.ctx.fillRect(0, 0, me.canvas.width, me.canvas.height);
+	//me.ctx.fillStyle = "rgba(0, 0, 0, 0)";
+	//me.ctx.fillStyle = 'black';
+	//me.ctx.clearRect(0, 0, canvas.width, canvas.height);
+	me.canvas.width = me.canvas.width;
+	//me.ctx.fillStyle = "rgba(0, 0, 0, 0.001)";
+	//me.ctx.fillRect(0, 0, me.canvas.width, me.canvas.height);
+	//me.ctx.font = "30px Arial";
+	//me.ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
 
 	//for each life
 	diamond.filter(function(life) {
@@ -93,22 +100,23 @@ var App = function(targetElementId, viewWidth, viewHeight, diamondHeight) {
 	    else {
 		me.ctx.fillStyle = 'Gray';
 	    }
-	    /*me.ctx.fillRect(life.col * _lifeHeight,
+	    /* me.ctx.fillRect(life.col * _lifeHeight,
 			    life.row * _lifeWidth, 
-			    _lifeWidth, _lifeHeight);*/
+			    _lifeWidth, _lifeHeight); */
 
 	    //var centerX = canvas.width / 2;
 	    //var centerY = canvas.height / 2;
 	    var radius = 8;
+	    timeSin = Math.sin(timeDiff);
 
 	    me.ctx.beginPath();
-	    me.ctx.arc(((life.col * _lifeHeight) + (me.canvas.width / 2) + (timeDiff * 100))
+	    
+	    me.ctx.arc(((life.col * _lifeHeight) + (me.canvas.width / 2) + (timeSin * 100))
 		       - ((life.row * _lifeHeight) / 2),
 		       life.row * _lifeWidth,
 		       radius, 0, 2 * Math.PI, false);
 	    me.ctx.fill();
 	    me.ctx.lineWidth = 0;
-
 
 	});
     };
