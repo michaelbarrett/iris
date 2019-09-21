5962072858912855d482aebd2ccecbec732b75e7
MichaelBarrett219 <michaelbarrett000@gmail.com>
2019-09-21 00:12:30 -0400

    alchemy table

--- js/pyr.js
+++ js/pyr.js
@@ -2,12 +2,12 @@ alert("working");
 
 var Alchemy = [
     [0, 0, 0, 0, 0, 0, 0],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    [0, 4, 3, 3, 4, 1, 1],
+    [0, 3, 2, 5, 6, 1, 1],
+    [0, 3, 5, 5, 5, 1, 1],
+    [0, 4, 6, 5, 3, 1, 1],
+    [0, 5, 5, 1, 1, 6, 1],
+    [0, 1, 1, 1, 1, 1, 2]
 ];
 
 var Life = function(row, col, _pyr) {
@@ -19,12 +19,13 @@ var Life = function(row, col, _pyr) {
 
     me.parents = null;
     me.seeFuture = function() {
-	return me.parents
+	//return the next state of the life based on parent states
     }
 
     return me;
 }
 
 var Field = function(height) {
-    
+    var me = this;
+    var _field = new Array();
 }
